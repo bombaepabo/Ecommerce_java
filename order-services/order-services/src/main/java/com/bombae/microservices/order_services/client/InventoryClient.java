07de118d96518e9b63cc07fcdeb08bff5e30db2a
baebom <inw11100@hotmail.com>
2025-10-23 23:00:29 +0700

    added circuitBreaker using Resilience4J

--- order-services/order-services/src/main/java/com/bombae/microservices/order_services/client/InventoryClient.java
+++ order-services/order-services/src/main/java/com/bombae/microservices/order_services/client/InventoryClient.java
@@ -1,12 +1,25 @@
 package com.bombae.microservices.order_services.client;
 
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
-
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name="inventory",fallbackMethod="fallbackMethod")
+    @Retry(name ="inventory")
     boolean isInStock(@RequestParam("sku_code") String sku_code,
                       @RequestParam("quantity") Integer quantity);
+
+    default boolean fallbackMethod(String code,Integer quantity,Throwable throwable){
+        log.info("Cannot get inventory for skucode {}, failure reason: {}", code,throwable.getMessage());
+        return false ;
+    }
+
 }
--- order-services/order-services/src/main/java/com/bombae/microservices/order_services/config/RestClientConfig.java
+++ order-services/order-services/src/main/java/com/bombae/microservices/order_services/config/RestClientConfig.java
@@ -4,9 +4,14 @@ import com.bombae.microservices.order_services.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,10 +23,20 @@ public class RestClientConfig {
     public InventoryClient inventoryClient(){
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
 
-}
+        private ClientHttpRequestFactory getClientRequestFactory() {
+        HttpClient httpClient = HttpClient.newBuilder()
+                .connectTimeout(Duration.ofSeconds(3))
+                .build();
+
+        JdkClientHttpRequestFactory factory = new JdkClientHttpRequestFactory(httpClient);
+        factory.setReadTimeout(Duration.ofSeconds(3));
+        return factory;
+    }
+    }
